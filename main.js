6038bb995d6965382bbbfd06128cc25dfbcf6ddd
Kingman9 <99269308+Kingman9@users.noreply.github.com>
2026-09-23 19:01:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -17358,3 +17358,6 @@ window.onload=function()
 		else loadLangAndLaunch(lang);
 	}
 };
+Game.registerHook('ready', function() {
+    Game.LoadMod('https://cookiemonsterteam.github.io/CookieMonster/dist/CookieMonster.js');
+}); 
